55d54161411a0677d647fb2957cb5c95ed07f94d
Ahmad-mustapha <ahmadishola12@gmail.com>
2023-11-29 16:49:28 +0100

    final

--- src/component/import.js
+++ src/component/import.js
@@ -4,4 +4,3 @@ export { default as WeAre} from './weAre/WeAre'
 export { default as NavBar} from './navbar/Navbar'
 export { default as Review} from './review/Review'
 export { default as WeWork} from './weWork/WeWork'
-
