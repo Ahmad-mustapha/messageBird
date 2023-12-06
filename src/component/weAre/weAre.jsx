f706254e9577b2459ef91c697d9d518b0e7e576f
Ahmad-mustapha <ahmadishola12@gmail.com>
2023-12-06 11:09:03 +0100

    fixes

--- src/component/weAre/weAre.jsx
+++ src/component/weAre/weAre.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import './weAre.css'
-function weAre({weAreImage, countryName}) {
-  return (
-    <div className='messageBird__weAre-container'>
-      <div className="messageBird__weAre-image">
-        <img src={weAreImage} />
-        <p>{countryName}</p>
-      </div>
-    </div>
-  )
-}
+// import React from 'react'
+// import './weAre.css'
+// function weAre({weAreImage, countryName}) {
+//   return (
+//     <div className='messageBird__weAre-container'>
+//       <div className="messageBird__weAre-image">
+//         <img src={weAreImage} />
+//         <p>{countryName}</p>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default weAre
+// export default weAre
--- src/container/whereWeAre/import.js
+++ src/container/whereWeAre/import.js
@@ -1,17 +1,17 @@
-import headPhone from '../../assets/Group.png'
-import alerm from '../../assets/Group (1).png'
-import spanner from '../../assets/Group (2).png'
-import box from '../../assets/Group (4).png'
-import wheel from '../../assets/Group (5).png'
-import chat from '../../assets/Group (3).png'
+// import headPhone from '../../assets/Group.png'
+// import alerm from '../../assets/Group (1).png'
+// import spanner from '../../assets/Group (2).png'
+// import box from '../../assets/Group (4).png'
+// import wheel from '../../assets/Group (5).png'
+// import chat from '../../assets/Group (3).png'
 
 
 
-export{
-    headPhone,
-    alerm,
-    spanner,
-    box,
-    wheel,
-    chat,
-}
+// export{
+//     headPhone,
+//     alerm,
+//     spanner,
+//     box,
+//     wheel,
+//     chat,
+// }
