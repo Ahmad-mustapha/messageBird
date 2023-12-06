14950bf94807fe2d4ec438e7e30869e86ca68f91
Ahmad-mustapha <ahmadishola12@gmail.com>
2023-12-06 11:19:27 +0100

    fixes

--- src/component/import.js
+++ src/component/import.js
@@ -1,6 +1,6 @@
 export { default as Footer} from './footer/Footer'
 export { default as Header} from './header/Header'
-export { default as WeAre} from './weAre/WeAre'
+// export { default as WeAre} from './weAre/WeAre'
 export { default as NavBar} from './navbar/Navbar'
 export { default as Review} from './review/Review'
 export { default as WeWork} from './weWork/WeWork'
